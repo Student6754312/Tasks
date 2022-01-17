4774496761d83adb01955dc74d76c47d613baa9b
konstantin <konstantin.kondrashov@st.oth-regensburg.de>
2022-01-17 21:45:51 +0100

    Added output of data fom input file  to output file

--- IOServices/InputFromFileService.cs
+++ IOServices/InputFromFileService.cs
@@ -6,16 +6,19 @@ namespace IOServices
     public class InputFromFileService<TA> : InputFromFileBaseService<TA> where TA: class 
     {
         private readonly OutputToConsoleService _outputService;
-        
-        public InputFromFileService(OutputToConsoleService outputService, IOptions<TA> options) : base(options)
+        private readonly IOutputService _outputFileService;
+
+        public InputFromFileService(OutputToConsoleService outputService, IOptions<TA> options, IOutputService outputFileService) : base(options)
         {
             _outputService = outputService;
+            _outputFileService = outputFileService;
         }
 
         public override string? Input()
         {
             var str = base.Input();
             _outputService.Output($"{str}\n");
+            _outputFileService.Output($"{str}\n");
             return str;
         }
 
